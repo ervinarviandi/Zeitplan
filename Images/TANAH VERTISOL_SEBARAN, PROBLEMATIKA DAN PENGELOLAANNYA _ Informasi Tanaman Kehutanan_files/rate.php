8afcb1999d20c53e6625333b675a18af131666ba
Ervin Arviandi <muhamadervin34@gmail.com>
2023-01-16 21:04:35 -0800

    update images 2

--- Images/TANAH VERTISOL_SEBARAN, PROBLEMATIKA DAN PENGELOLAANNYA _ Informasi Tanaman Kehutanan_files/rate.php
+++
@@ -1,6 +0,0 @@
-PDRTJS_4512181_post_462.avg_rating = 5;
-PDRTJS_4512181_post_462.votes = 1;
-PDRTJS_settings_4512181_post_462= {"type":"stars","size":"sml","star_color":"yellow","custom_star":"","font_size":"","font_line_height":"16px","font_color":"","font_align":"left","font_position":"right","font_family":"","font_bold":"normal","font_italic":"normal","text_vote":"Vote","text_votes":"Votes","text_rate_this":"Rate This","text_1_star":"Very Poor","text_2_star":"Poor","text_3_star":"Average","text_4_star":"Good","text_5_star":"Excellent","text_thank_you":"Thank You","text_rate_up":"Rate Up","text_rate_down":"Rate Down"};
-PDRTJS_4512181_post_462.init( {"type":"stars","size":"sml","star_color":"yellow","custom_star":"","font_size":"","font_line_height":"16px","font_color":"","font_align":"left","font_position":"right","font_family":"","font_bold":"normal","font_italic":"normal","text_vote":"Vote","text_votes":"Votes","text_rate_this":"Rate This","text_1_star":"Very Poor","text_2_star":"Poor","text_3_star":"Average","text_4_star":"Good","text_5_star":"Excellent","text_thank_you":"Thank You","text_rate_up":"Rate Up","text_rate_down":"Rate Down"} );
-PDRTJS_4512181_post_462.token='08777cf42c589616b9173357be5c1b1b';
-/*4512181,_post_462,wp-post-462,1740517231,5-1*/
